c30f15984cf0a6377d3b083a151c80761d647b64
DaTea83 <eugenechoy83@gmail.com>
2026-05-02 20:34:05 +0800

    Added fonts

---
+++ Assets/GPUAnimationForDOTS/Source/Editor/ToolkitData.cs
@@ -0,0 +1,49 @@
+﻿// Copyright 2026 DaTea83
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using GPUAnimationForDOTS.Source.Editor.Utils;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace GPUAnimationForDOTS.Source.Editor {
+    
+    public readonly struct ToolkitData {
+        private readonly string _uxmlPath;
+        private readonly string _ussPath;
+        
+        private VisualTreeAsset Tree => AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(_uxmlPath);
+        private StyleSheet Style => AssetDatabase.LoadAssetAtPath<StyleSheet>(_ussPath);
+
+        public ToolkitData(string name) {
+            var path = EditorCollection.FindPathByName(name, "VisualTreeAsset");
+            this._uxmlPath = path;
+            this._ussPath = path.Replace(".uxml", ".uss");
+        }
+        
+        public VisualElement Clone(VisualElement root = null) {
+            if (Tree is null) throw new InvalidOperationException("VisualTreeAsset not found");
+            if (Style is null) throw new InvalidOperationException("StyleSheet not found");
+
+            if (root == null) {
+                Tree.CloneTree();
+            }
+            else {
+                Tree.CloneTree(root);
+                root.styleSheets.Add(Style);
+            }
+            return root;
+        }
+    }
+}
---
+++ Assets/GPUAnimationForDOTS/Source/Editor/Utils/EditorCollection.cs
@@ -0,0 +1,34 @@
+﻿// Copyright 2026 DaTea83
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace GPUAnimationForDOTS.Source.Editor.Utils {
+    
+    internal static class EditorCollection {
+        
+        public static string ProjectFolderName => Directory.GetParent(Application.dataPath)?.Name;
+
+        public static string FindPathByName(string name, string type = "script") {
+            var guids = AssetDatabase.FindAssets($"{name} t:{type}");
+
+            if (guids is null || guids.Length == 0)
+                return string.Empty;
+
+            return AssetDatabase.GUIDToAssetPath(guids[0]);
+        }
+    }
+}
